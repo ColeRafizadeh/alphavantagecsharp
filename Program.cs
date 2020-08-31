7ba7e526a20ccee3939954cbe0a60b05a33e54a0
Cole <crafiza1@uncc.edu>
2020-08-31 14:31:43 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace rest
+{
+
+    class Program
+    {
+        public class AVData
+        {
+            public DateTime time { get; set; }
+            public DateTime Open { get; set; }
+            public DateTime High { get; set; }
+            public DateTime Low { get; set; }
+            public DateTime Close { get; set; }
+            public DateTime Volume { get; set; }
+        }
+        public static void QuestionOne()
+        {
+            string key = "650CS2ENBH9B4PB1";
+            var client = new RestClient("https://www.alphavantage.co/query?");
+            var request = new RestRequest(Method.GET);
+            request.AddParameter("symbol", "MSFT");
+            request.AddParameter("apikey", key);
+            request.AddParameter("function", "TIME_SERIES_DAILY");
+            request.AddParameter("outputsize", "compact");
+            IRestResponse response = client.Execute(request);
+
+            Console.WriteLine(JObject.Parse(response.Content)["Meta Data"].ToObject<Dictionary<string, string>>());
+
+        }
+
+        static public void Main(String[] args)
+        {
+            QuestionOne();
+        }
+    }
+}

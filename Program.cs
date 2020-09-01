4e1966146ba48ad0d9d57ffdae06273f5e886370
Cole <crafiza1@uncc.edu>
2020-09-01 11:59:13 -0400

    json

--- Program.cs
+++ Program.cs
@@ -1,36 +1,77 @@
 ﻿using System;
-using ServiceStack.Text;
+using System.Collections.Generic;
+using models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
+
+
+namespace models
+{
+
+    public class StockData
+    {
+        [JsonProperty("Meta Data")]
+        public MetaData MetaData { get; set; }
+
+        [JsonProperty("Weekly Time Series")]
+        public Dictionary<string, WeeklyTimeSery> WeeklyTimeSeries { get; set; }
+    }
+
+    public class MetaData
+    {
+        [JsonProperty("1. Information")]
+        public string The1Information { get; set; }
+
+        [JsonProperty("2. Symbol")]
+        public string The2Symbol { get; set; }
+
+        [JsonProperty("3. Last Refreshed")]
+        public DateTimeOffset The3LastRefreshed { get; set; }
+
+        [JsonProperty("4. Time Zone")]
+        public string The4TimeZone { get; set; }
+    }
+
+    public class WeeklyTimeSery
+    {
+        [JsonProperty("1. open")]
+        public string The1Open { get; set; }
+
+        [JsonProperty("2. high")]
+        public string The2High { get; set; }
+
+        [JsonProperty("3. low")]
+        public string The3Low { get; set; }
+
+        [JsonProperty("4. close")]
+        public string The4Close { get; set; }
+
+        [JsonProperty("5. volume")]
+        public long The5Volume { get; set; }
+    }
+}
 
 namespace rest
 {
 
     class Program
     {
-        public class AVData
-        {
-            public DateTime time { get; set; }
-            public Decimal Open { get; set; }
-            public Decimal High { get; set; }
-            public Decimal Low { get; set; }
-            public Decimal Close { get; set; }
-            public Decimal Volume { get; set; }
-        }
         public static void QuestionOne()
         {
-            string key = "";
+            string ticker = "msft";
+            string key = "650CS2ENBH9B4PB1";
             var client = new RestClient("https://www.alphavantage.co/query?");
             var request = new RestRequest(Method.GET);
-            request.AddParameter("symbol", "MSFT");
+            request.AddParameter("symbol", ticker);
             request.AddParameter("apikey", key);
             request.AddParameter("function", "TIME_SERIES_DAILY");
             request.AddParameter("outputsize", "compact");
-            request.AddParameter("datatype", "csv");
+            request.AddParameter("datatype", "json");
             IRestResponse response = client.Execute(request);
+            StockData data = JsonConvert.DeserializeObject<StockData>(response.Content);
 
-            var timeseries 
-            Console.WriteLine("Begin");
-            Console.WriteLine(response.Content);
+
             
 
         }

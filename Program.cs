aa2ad8cf614562e39d869b0e303d74fee306b18f
Cole <crafiza1@uncc.edu>
2020-09-02 08:14:46 -0400

    first two

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -13,7 +14,6 @@ namespace models
         public MetaData MetaData { get; set; }
         [JsonProperty("Time Series (Daily)")]
         public Dictionary<DateTime, DailyData> TimeSeries { get; set; }
-        public List<DailyData> TimeSeriesList() => new List<DailyData>(TimeSeries.Values);
     }
     public class MetaData
     {
@@ -38,27 +38,30 @@ namespace models
         public double Close { get; set; }
         [JsonProperty("5. volume")]
         public long Volume { get; set; }
+
+        public double Change() { return Open - Close; }
     }
 }
- 
 class Program
 {
     public static String api_key = "650CS2ENBH9B4PB1";
 
-    public static StockData getStock(String symbol)
+    //Full = Everything, Compact = last 100 
+    public static StockData getStock(String symbol, Boolean full)
     {
         var request = new RestRequest(Method.GET).AddParameter("symbol", symbol)
             .AddParameter("apikey", api_key)
             .AddParameter("function", "TIME_SERIES_DAILY")
-            .AddParameter("outputsize", "full")
+            .AddParameter("outputsize", full ? "full" :"compact")
             .AddParameter("datatype", "json");
         var response = new RestClient("https://www.alphavantage.co/query?").Execute(request);
         return JsonConvert.DeserializeObject<StockData>(response.Content);
     }
+
     public static void QuestionOne()
     {
         
-        var msft = getStock("msft");
+        var msft = getStock("msft", false);
         var startRange = DateTime.Today.AddDays(-7);
         var endRange = DateTime.Today;
         var sum = 0;
@@ -77,9 +80,9 @@ class Program
     }
     public static void QuestionTwo()
     {
-        var aapl = getStock("aapl");
-        DateTime startRange = DateTime.Today.AddDays(-183);
-        DateTime endRange = DateTime.Today;
+        var aapl = getStock("aapl", true);
+        var startRange = DateTime.Today.AddDays(-183);
+        var endRange = DateTime.Today;
         double highest = 0;
 
         foreach (var e in aapl.TimeSeries)
@@ -94,10 +97,43 @@ class Program
         }
         Console.WriteLine("Highest closing price of " + aapl.MetaData.Symbol + " is " + highest);
     }
-
+    public static void QuestionThree()
+    {
+        var ba = getStock("ba", false);
+        var startRange = DateTime.Today.AddDays(-31);
+        var endRange = DateTime.Today;
+        foreach (var e in ba.TimeSeries)
+        {
+            if (e.Key >= startRange && e.Key <= endRange)
+            {
+                Console.WriteLine(e.Key.ToShortDateString() + ": " + ba.MetaData.Symbol + " price changed: " + e.Value.Change());
+            }
+        }
+    }
+    public static void QuestionFour(String[] symbols)
+    {
+        StockData roi = null;
+        var highest = 0.0;
+        foreach(String s in symbols)
+        {
+            var stock = getStock(s, false);
+            Console.WriteLine(stock.MetaData.Symbol);
+            var list = stock.TimeSeries.Values.ToList();
+            var ret = list.ElementAt(0).Close - list.ElementAt(31).Close;
+            if(ret > highest)
+            {
+                highest = ret;
+                roi = stock;
+            }
+        }
+        Console.WriteLine("Highest ROI was " + roi.MetaData.Symbol + " with a $" + highest + " return");
+    }
     static public void Main(String[] args)
     {
         QuestionOne();
         QuestionTwo();
+        QuestionThree();
+        String[] symbols = {"msft","ba"};
+        QuestionFour(symbols);
     }
 }
